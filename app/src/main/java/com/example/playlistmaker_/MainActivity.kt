a947cff8df97ae41b561105988035da57240aa3f
user <vladislavkhamin90@gmail.com>
2025-02-07 11:50:45 +0300

    add Toast

--- app/src/main/java/com/example/playlistmaker_/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker_/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.playlistmaker_
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +18,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        searchButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Вы нажали на кнопку 'Поиск'", Toast.LENGTH_SHORT).show()
+        }
+        mediaButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Вы нажали на кнопку 'Медиатека'", Toast.LENGTH_SHORT).show()
+        }
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Вы нажали на кнопку 'Настройки'", Toast.LENGTH_SHORT).show()
         }
 
     }
